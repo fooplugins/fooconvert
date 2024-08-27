1dfa331a174cbdf89b2c0353bebe99a5fc764597
Steve Usher <steveush@gmail.com>
2024-08-27 18:44:33 +0200

    Updated gulp file to remove translations tasks as this is now performed using the wp-cli. See the package script 'make-pot'.

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,30 @@
-const gulp = require("gulp");
-const gulpWpPot = require("gulp-wp-pot");
-const gulpZip = require("gulp-zip");
-const gulpFreemius = require("gulp-freemius-deploy");
-const del = require("del");
-const pkg = require("./package.json");
-const freemiusConfig = require("./fs-config.json");
+const gulp = require( "gulp" );
+const gulpZip = import( "gulp-zip" );
+const gulpFreemius = require( "gulp-freemius-deploy" );
+const del = require( "del" );
+const pkg = require( "./package.json" );
+const freemiusConfig = require( "./fs-config.json" );
 
 // register the freemius-deploy task
-gulpFreemius(gulp, {
+gulpFreemius( gulp, {
     ...freemiusConfig,
-    zip_name: `${pkg.name}.v${pkg.version}.zip`,
+    zip_name: `${ pkg.name }.v${ pkg.version }.zip`,
     zip_path: "./dist/",
     add_contributor: true
-});
+} );
 
 // clean up the files created by the tasks
-function clean(){
-    return del([
-        `./languages/${pkg.name}.pot`,
-        `./dist/${pkg.name}.v${pkg.version}.zip`
-    ]);
-}
-
-// extract a .pot file from all PHP files excluding those in the node_modules dir
-function translate(){
-    return gulp.src("./**/*.php")
-        .pipe(gulpWpPot({
-            "domain": `${pkg.name}`,
-            "package": `${pkg.title}`,
-            "bugReport": `${pkg.bugs}`,
-            "team": `${pkg.author}`,
-            "lastTranslator": `${pkg.author}`
-        }))
-        .pipe(gulp.dest(`./languages/${pkg.name}.pot`));
+function clean() {
+    return del( [ `./dist/${ pkg.name }.v${ pkg.version }.zip` ] );
 }
 
 // create a .zip containing just the production code for the plugin
-function zip(){
-    return gulp.src([
-        "**/*",
-        "!package*.json",
-        "!./{node_modules,node_modules/**/*}",
-        "!./{dist,dist/**/*}",
-        "!./{src,src/**/*}",
-        '!./{gutenberg/src,gutenberg/src/**/*,gutenberg/config,gutenberg/config/**/*}',
-        "!fs-config.json",
-        "!composer.json",
-        "!composer.lock",
-        "!gulpfile*.js",
-        "!webpack*.js",
-        "!./{gulpfile.js,gulpfile.js/**/*}",
-        '!README.md',
-        '!Gruntfile.js'
-    ])
-        .pipe(gulpZip(`${pkg.name}.v${pkg.version}.zip`))
-        .pipe(gulp.dest("./dist"));
+function zip() {
+    return gulpZip.then( module => {
+        return gulp.src( pkg.files )
+            .pipe( module?.default(`${ pkg.name }.v${ pkg.version }.zip`) )
+            .pipe( gulp.dest( "./dist" ) );
+    } );
 }
 
-exports.default = gulp.series(clean, translate, zip);
+exports.default = gulp.series( clean, zip );
