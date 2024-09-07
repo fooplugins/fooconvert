88d9c19c4530eaf3c38989d83ecece4afcee4eea
Steve Usher <steveush@gmail.com>
2024-09-07 18:35:28 +0200

    Bumped dev version.

--- assets/widgets/bar/editor/content/index.asset.php
+++ assets/widgets/bar/editor/content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('fc-editor'), 'version' => '14c5fc75f4049564412d');
+<?php return array('dependencies' => array('fc-editor'), 'version' => '16ebc02459bf5cfdacb5');
