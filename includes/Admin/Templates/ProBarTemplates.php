f63ff6446654a25bfcd53b5bc9b6a6e01110d6f7
Brad Vincent <bradvin@gmail.com>
2025-07-14 13:25:19 +0100

    updated promos for templates

--- includes/Admin/Templates/ProBarTemplates.php
+++ includes/Admin/Templates/ProBarTemplates.php
@@ -5,38 +5,38 @@
  */
 return array(
     array(
-        'name' => 'bar__digital_download_signup',
-        'title' => __( 'Digital Download Signup', 'fooconvert' ),
-        'description' => __( 'The "Digital Download Signup" bar template is a lead magnet help you grow your mailing list by offering a free download (e.g. an e-book, PDF, etc). All leads are also captured under FooConvert -> Leads.', 'fooconvert' ),
-        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__digital_download_signup.png',
-        'pro' => true
-    ),
-    array(
-        'name' => 'bar__newsletter_subscribe',
-        'title' => __( 'Newsletter Subscribe', 'fooconvert' ),
-        'description' => __( 'Newsletter Subscribe marketing themed bar.', 'fooconvert' ),
-        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__newsletter_subscribe.png',
-        'pro' => true,
-    ),
-    array(
-        'name' => 'bar__smart_exit_offer',
-        'title' => __( 'Smart Exit Offer', 'fooconvert' ),
-        'description' => __( 'A smart exit offer bar lead magnet to help prevent visitors from leaving your site.', 'fooconvert' ),
-        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__smart_exit_offer.png',
+        'name' => 'bar__watch_the_video',
+        'title' => __( 'Watch the Video', 'fooconvert' ),
+        'description' => __( 'The "Watch the Video" bar templates guides visitors to watch a specific video. Get more views to your YouTube channel!', 'fooconvert' ),
+        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__video.png',
         'pro' => true,
     ),
     array(
         'name' => 'bar__special_offer',
         'title' => __( 'Special Offer Countdown', 'fooconvert' ),
-        'description' => __( 'Guide visitors to specific offer with a countdown timer.', 'fooconvert' ),
+        'description' => __( 'The "Special Offer Countdown" bar templates guides visitors to a specific offer with an awesome looking countdown timer.', 'fooconvert' ),
         'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__special_offer.png',
         'pro' => true,
     ),
     array(
-        'name' => 'bar__watch_the_video',
-        'title' => __( 'Watch the Video', 'fooconvert' ),
-        'description' => __( 'Guide visitors to a watch a specific video.', 'fooconvert' ),
-        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__video.png',
+        'name' => 'bar__digital_download_signup',
+        'title' => __( 'Digital Download Signup', 'fooconvert' ),
+        'description' => __( 'The "Digital Download Signup" bar template is a lead magnet to help you grow your mailing list by offering a free download (e.g. an e-book, PDF, etc). All leads are also captured under FooConvert -> Leads.', 'fooconvert' ),
+        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__digital_download_signup.png',
+        'pro' => true
+    ),
+    array(
+        'name' => 'bar__smart_exit_offer',
+        'title' => __( 'Smart Exit Offer', 'fooconvert' ),
+        'description' => __( 'The "Smart Exit Offer" bar templates is an exit intent bar that guides visitors to copy and use a specific coupon code to help persuade visitors to stay on your site.', 'fooconvert' ),
+        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__smart_exit_offer.png',
         'pro' => true,
     ),
+    array(
+        'name' => 'bar__newsletter_subscribe',
+        'title' => __( 'Newsletter Subscribe', 'fooconvert' ),
+        'description' => __( 'The "Newsletter Subscribe" bar template captures leads for your newsletter. All leads are also captured under FooConvert -> Leads.', 'fooconvert' ),
+        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__newsletter_subscribe.png',
+        'pro' => true,
+    )
 );
--- includes/Admin/Templates/ProPopupTemplates.php
+++ includes/Admin/Templates/ProPopupTemplates.php
@@ -5,51 +5,38 @@
  */
 return array(
     array(
-        'name' => 'popup__digital_download_signup',
-        'title' => __( 'Digital Download Signup', 'fooconvert' ),
-        'description' => __( 'Digital download signup themed popup.', 'fooconvert' ),
-        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__digital_download_signup.png',
-        'pro' => true,
-        'upsell' => array(
-            'title' => 'Digital Download Signup',
-            'content' => '<p>example description <a href="#link">with a link</a></p>',
-            'image' => FOOCONVERT_ASSETS_URL . 'media/templates/template__digital_download_signup.png',
-            'primary' => array(
-                'text' => 'View PRO Pricing',
-                'href' => '#view_pro_pricing_url',
-            ),
-            'secondary' => array(
-                'text' => 'Start Free Trial',
-                'href' => '#start_free_trial_url',
-            )
-        )
-    ),
-    array(
-        'name' => 'popup__newsletter_subscribe',
-        'title' => __( 'Newsletter Subscribe', 'fooconvert' ),
-        'description' => __( 'Newsletter Subscribe marketing themed popup.', 'fooconvert' ),
-        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__newsletter_subscribe.png',
-        'pro' => true,
-    ),
-    array(
-        'name' => 'popup__smart_exit_offer',
-        'title' => __( 'Smart Exit Offer', 'fooconvert' ),
-        'description' => __( 'Smart exit offer themed popup.', 'fooconvert' ),
-        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__smart_exit_offer.png',
+        'name' => 'popup__watch_the_video',
+        'title' => __( 'Watch the Video', 'fooconvert' ),
+        'description' => __( 'The "Watch the Video" popup templates guides visitors to watch a specific video. Get more views to your YouTube channel!', 'fooconvert' ),
+        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__video.png',
         'pro' => true,
     ),
     array(
         'name' => 'popup__special_offer',
         'title' => __( 'Special Offer Countdown', 'fooconvert' ),
-        'description' => __( 'Guide visitors to specific offer with a countdown timer.', 'fooconvert' ),
+        'description' => __( 'The "Special Offer Countdown" popup templates guides visitors to a specific offer with an awesome looking countdown timer.', 'fooconvert' ),
         'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__special_offer.png',
         'pro' => true,
     ),
     array(
-        'name' => 'popup__watch_the_video',
-        'title' => __( 'Watch the Video', 'fooconvert' ),
-        'description' => __( 'Guide visitors to a watch a specific video.', 'fooconvert' ),
-        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__video.png',
+        'name' => 'popup__digital_download_signup',
+        'title' => __( 'Digital Download Signup', 'fooconvert' ),
+        'description' => __( 'The "Digital Download Signup" popup template is a lead magnet to help you grow your mailing list by offering a free download (e.g. an e-book, PDF, etc). All leads are also captured under FooConvert -> Leads.', 'fooconvert' ),
+        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__digital_download_signup.png',
+        'pro' => true
+    ),
+    array(
+        'name' => 'popup__smart_exit_offer',
+        'title' => __( 'Smart Exit Offer', 'fooconvert' ),
+        'description' => __( 'The "Smart Exit Offer" popup templates is an exit intent bar that guides visitors to copy and use a specific coupon code to help persuade visitors to stay on your site.', 'fooconvert' ),
+        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__smart_exit_offer.png',
         'pro' => true,
     ),
+    array(
+        'name' => 'popup__newsletter_subscribe',
+        'title' => __( 'Newsletter Subscribe', 'fooconvert' ),
+        'description' => __( 'The "Newsletter Subscribe" popup template captures leads for your newsletter. All leads are also captured under FooConvert -> Leads.', 'fooconvert' ),
+        'thumbnail' => FOOCONVERT_ASSETS_URL . 'media/templates/template__newsletter_subscribe.png',
+        'pro' => true,
+    )
 );
