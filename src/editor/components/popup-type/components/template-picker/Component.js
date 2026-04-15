144935380169a52f2587ed7a279aa8d0add00676
Brad Vincent <bradvin@gmail.com>
2026-04-15 21:02:26 +0100

    Refine popup type tabs

--- src/editor/components/popup-type/components/template-picker/Component.js
+++ src/editor/components/popup-type/components/template-picker/Component.js
@@ -1,5 +1,4 @@
 import { createBlock } from "@wordpress/blocks";
-import { Button, ButtonGroup } from "@wordpress/components";
 import { useDispatch, useSelect } from "@wordpress/data";
 import { store as editorStore } from "@wordpress/editor";
 import { useEffect } from "@wordpress/element";
@@ -27,6 +26,8 @@ const POPUP_TYPE_OPTIONS = [
         blockName: "fc/overlay",
     },
 ];
+
+const ROOT_CLASS = "fc--popup-type-template-picker";
 
 const PopupTypeTemplatePicker = ( {
     clientId,
@@ -72,44 +73,118 @@ const PopupTypeTemplatePicker = ( {
         resetEditorBlocks( [ createBlock( nextOption.blockName ) ] );
     };
 
+    const focusTab = nextPopupType => {
+        if ( typeof document?.getElementById !== "function" ) {
+            return;
+        }
+
+        const element = document.getElementById( `${ ROOT_CLASS }__tab-${ nextPopupType }` );
+        if ( element instanceof HTMLElement ) {
+            element.focus();
+        }
+    };
+
+    const moveTabSelection = offset => {
+        const currentIndex = POPUP_TYPE_OPTIONS.findIndex( option => option.value === currentPopupType );
+        if ( currentIndex === -1 ) {
+            return;
+        }
+
+        const nextIndex = ( currentIndex + offset + POPUP_TYPE_OPTIONS.length ) % POPUP_TYPE_OPTIONS.length;
+        const nextPopupType = POPUP_TYPE_OPTIONS[ nextIndex ]?.value;
+        if ( typeof nextPopupType !== "string" ) {
+            return;
+        }
+
+        switchPopupType( nextPopupType );
+        window?.requestAnimationFrame?.( () => focusTab( nextPopupType ) );
+    };
+
+    const handleTabKeyDown = event => {
+        switch ( event.key ) {
+            case "ArrowLeft":
+            case "ArrowUp":
+                event.preventDefault();
+                moveTabSelection( -1 );
+                break;
+            case "ArrowRight":
+            case "ArrowDown":
+                event.preventDefault();
+                moveTabSelection( 1 );
+                break;
+            case "Home":
+                event.preventDefault();
+                switchPopupType( POPUP_TYPE_OPTIONS[0].value );
+                window?.requestAnimationFrame?.( () => focusTab( POPUP_TYPE_OPTIONS[0].value ) );
+                break;
+            case "End": {
+                event.preventDefault();
+                const lastPopupType = POPUP_TYPE_OPTIONS[ POPUP_TYPE_OPTIONS.length - 1 ].value;
+                switchPopupType( lastPopupType );
+                window?.requestAnimationFrame?.( () => focusTab( lastPopupType ) );
+                break;
+            }
+        }
+    };
+
+    const activeOption = POPUP_TYPE_OPTIONS.find( option => option.value === currentPopupType ) ?? POPUP_TYPE_OPTIONS[0];
+    const panelId = `${ ROOT_CLASS }__panel-${ activeOption.value }`;
+    const activeTabId = `${ ROOT_CLASS }__tab-${ activeOption.value }`;
+
     const tabs = (
-        <div className="fc--popup-type-template-picker__header">
-            <label className="fc--popup-type-template-picker__label">
+        <div className={ `${ ROOT_CLASS }__header` }>
+            <div className={ `${ ROOT_CLASS }__label` }>
                 { __( "Popup type", "fooconvert" ) }
-            </label>
-            <ButtonGroup
-                className="fc--popup-type-template-picker__tabs"
+            </div>
+            <div
+                className={ `${ ROOT_CLASS }__tabs nav-tab-wrapper` }
+                role="tablist"
                 aria-label={ __( "Popup type", "fooconvert" ) }
             >
                 { POPUP_TYPE_OPTIONS.map( option => {
                     const isActive = option.value === currentPopupType;
+                    const tabId = `${ ROOT_CLASS }__tab-${ option.value }`;
 
                     return (
-                        <Button
+                        <button
+                            type="button"
                             key={ option.value }
-                            variant="tertiary"
-                            isPressed={ isActive }
+                            id={ tabId }
+                            role="tab"
+                            className={ `${ ROOT_CLASS }__tab nav-tab${ isActive ? " nav-tab-active" : "" }` }
+                            aria-selected={ isActive }
+                            aria-controls={ panelId }
+                            tabIndex={ isActive ? 0 : -1 }
                             onClick={ () => switchPopupType( option.value ) }
-                            aria-pressed={ isActive }
+                            onKeyDown={ handleTabKeyDown }
                         >
                             { option.label }
-                        </Button>
+                        </button>
                     );
                 } ) }
-            </ButtonGroup>
+            </div>
         </div>
     );
 
     return (
-        <VariationPicker
-            { ...props }
-            clientId={ clientId }
-            beforeToolbar={ tabs }
-            label={ __( "Choose a template", "fooconvert" ) }
-            reset={ { template: undefined } }
-            media="thumbnail"
-            className="fc--popup-type-template-picker"
-        />
+        <div className={ ROOT_CLASS }>
+            <div
+                id={ panelId }
+                className={ `${ ROOT_CLASS }__panel` }
+                role="tabpanel"
+                aria-labelledby={ activeTabId }
+            >
+                <VariationPicker
+                    { ...props }
+                    clientId={ clientId }
+                    beforeToolbar={ tabs }
+                    label={ __( "Choose a template", "fooconvert" ) }
+                    reset={ { template: undefined } }
+                    media="thumbnail"
+                    className={ `${ ROOT_CLASS }__picker` }
+                />
+            </div>
+        </div>
     );
 };
 
