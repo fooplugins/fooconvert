3ff7a74caaa65b7f4d52f437a7b12040d654930c
Brad Vincent <bradvin@gmail.com>
2026-05-13 15:16:23 +0100

    Fix parent attribute hook stability

--- src/editor/hooks/useParentAttributes.js
+++ src/editor/hooks/useParentAttributes.js
@@ -1,6 +1,8 @@
-import { useDispatch, useSelect, select, dispatch } from "@wordpress/data";
+import { useCallback } from "@wordpress/element";
+import { useDispatch, useSelect } from "@wordpress/data";
 import { store as blockEditorStore } from "@wordpress/block-editor";
-import { cleanObject, isArray } from "@steveush/utils";
+
+const EMPTY_OBJECT = {};
 
 /**
  *
@@ -10,43 +12,56 @@ import { cleanObject, isArray } from "@steveush/utils";
  * @returns {[object,((value:(object|undefined))=>void),string]|[]}
  */
 const useParentAttributes = ( clientId, parentName, last = false ) => {
-    const parents = useSelect(
+    const parentClientId = useSelect(
         select => {
             const blockEditorSelect = select( blockEditorStore );
             const getBlockName = blockEditorSelect?.getBlockName;
             const getBlockParents = blockEditorSelect?.getBlockParents;
 
             if ( typeof getBlockName !== "function" || typeof getBlockParents !== "function" ) {
-                return [];
+                return "";
             }
 
-            return ( getBlockParents( clientId ) ?? [] ).filter( parentClientId => {
+            const parents = ( getBlockParents( clientId ) ?? [] ).filter( parentClientId => {
                 return getBlockName( parentClientId ) === parentName;
             } );
+
+            if ( parents.length === 0 ) {
+                return "";
+            }
+
+            return last ? parents[ 0 ] : parents[ parents.length - 1 ];
         },
-        [ clientId, parentName ]
+        [ clientId, parentName, last ]
     );
-    if ( isArray( parents, true ) ) {
-        const parentClientId = last ? parents.shift() : parents.pop();
-        if ( typeof parentClientId === 'string' ) {
-            const attributes = select( blockEditorStore )?.getBlockAttributes( parentClientId ) ?? {};
-            const setAttributes = value => {
-                const nextValue = typeof value === 'object' ? value : {};
-                dispatch( blockEditorStore )?.updateBlockAttributes( parentClientId, nextValue );
-            };
-            return [ attributes, setAttributes, parentClientId ];
-        }
+
+    const attributes = useSelect(
+        select => {
+            if ( typeof parentClientId !== "string" || parentClientId === "" ) {
+                return EMPTY_OBJECT;
+            }
+
+            return select( blockEditorStore )?.getBlockAttributes( parentClientId ) ?? EMPTY_OBJECT;
+        },
+        [ parentClientId ]
+    );
+    const { updateBlockAttributes } = useDispatch( blockEditorStore );
+    const setAttributes = useCallback(
+        value => {
+            if ( typeof parentClientId !== "string" || parentClientId === "" ) {
+                return;
+            }
+
+            const nextValue = typeof value === 'object' ? value : {};
+            updateBlockAttributes( parentClientId, nextValue );
+        },
+        [ parentClientId, updateBlockAttributes ]
+    );
+
+    if ( typeof parentClientId === "string" && parentClientId !== "" ) {
+        return [ attributes, setAttributes, parentClientId ];
     }
-    // const attributes = useSelect( select => select( blockEditorStore )?.getBlockAttributes( parentClientId ) ?? {}, [ parentClientId ] );
-    // const { updateBlockAttributes } = useDispatch( blockEditorStore );
-    // if ( typeof parentClientId !== 'string' ) {
-    //     return [];
-    // }
-    // const setAttributes = value => {
-    //     let nextValue = typeof value === 'object' ? value : {};
-    //     // noinspection JSIgnoredPromiseFromCall,JSCheckFunctionSignatures
-    //     updateBlockAttributes( clientId, nextValue, false );
-    // };
+
     return [];
 };
 
